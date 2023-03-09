bbaa2e396a2dea8b177176bab627235593fb0332
Bruno Russo <russo.bruno99@gmail.com>
2023-03-09 11:23:18 +0000

    Initial Commit

---
+++ PVGym/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PVGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
