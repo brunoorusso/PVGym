bddd523b87d029287489c555c4d0109680d5c78b
brunoorusso <russo.bruno99@gmail.com>
2023-04-28 00:15:51 +0100

    Register & Profile comentado

--- PVGymAngularClient/ClientApp/src/app/register/register.component.ts
+++ PVGymAngularClient/ClientApp/src/app/register/register.component.ts
@@ -1,3 +1,8 @@
+/*
+ * Author: Bruno Russo
+ * Description: This component is responsible for the add member page.
+ * It uses the user service by injection to register a new member.
+ */ 
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { UserService } from '../user.service';
@@ -10,19 +15,25 @@ import { UserService } from '../user.service';
 })
 export class RegisterComponent implements OnInit {
 
-  /*
-   * Autor: Bruno Russo
-   */
-
   constructor(public service: UserService) {
   }
 
+  /*
+   * On Init Method
+   * When the component is initialized, the associated formModel is reseted.
+   * The field planType is set to normal by default.
+   */ 
   ngOnInit(): void {
     this.service.formModel.reset();
     this.service.formModel.get('planType')?.setValue('normal');
     
   }
 
+  /*
+   * On Submit Method
+   * When the form is submitted, the register method of the user service is called.
+   * After that, the associated formModel is reseted.
+   */ 
   onSubmit() {
     this.service.register().subscribe();
     this.service.formModel.reset();
