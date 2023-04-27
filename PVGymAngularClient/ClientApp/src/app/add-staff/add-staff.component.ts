28c3b930d0e4b81f97f6cdc29bfc553b730105ef
brunoorusso <russo.bruno99@gmail.com>
2023-04-28 00:22:13 +0100

    Login & Add Staff comentado

--- PVGymAngularClient/ClientApp/src/app/add-staff/add-staff.component.ts
+++ PVGymAngularClient/ClientApp/src/app/add-staff/add-staff.component.ts
@@ -1,3 +1,8 @@
+/*
+ * Author: Bruno Russo
+ * Description: This component is responsible for the add staff page.
+ * It uses the user service by injection to register a new staff.
+ */
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 
@@ -7,15 +12,21 @@ import { UserService } from '../user.service';
   styleUrls: ['./add-staff.component.css']
 })
 export class AddStaffComponent implements OnInit {
-  /*
-   * Autor: Bruno Russo
-   */
+  
   constructor(public service: UserService) { }
 
+  /*
+   * ngOnInit Method
+   * When the component is initialized, the associated formModel is reseted.
+   */
   ngOnInit(): void {
-    this.service.formModel.reset();
+    this.service.staffFormModel.reset();
   }
 
+  /*
+   * onSubmit Method
+   * When the form is submitted, the addStaff method of the user service is called. 
+   */
   onSubmit() {
     this.service.addStaff().subscribe();
     this.service.staffFormModel.reset();
--- PVGymAngularClient/ClientApp/src/app/login/login.component.ts
+++ PVGymAngularClient/ClientApp/src/app/login/login.component.ts
@@ -1,3 +1,8 @@
+/*
+ * Author: Bruno Russo
+ * Description: This component is responsible for the login page.
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
@@ -13,11 +18,19 @@ export class LoginComponent implements OnInit {
 
   constructor(public service: UserService, private router: Router) { }
 
+  /*
+   * ngOnInit method
+   * When the component is initialized, the login form is reseted.
+   */ 
   ngOnInit(): void {
     this.service.loginFormModel.reset();
-    
   }
 
+  /*
+   * onSubmit method
+   * When the user submits the login form, the login method is called.
+   * If the login is sucessfull, a token is assigned to the user.
+   */ 
   onSubmit() {
     this.service.login().subscribe(
       (result: any) => {
