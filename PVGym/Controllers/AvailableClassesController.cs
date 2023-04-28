32a94b77bb9d989c2fcb96c815ef387f241da647
ismpro <38300904+ismpro@users.noreply.github.com>
2023-04-28 14:05:17 +0100

    available classes

--- PVGym/Controllers/AvailableClassesController.cs
+++ PVGym/Controllers/AvailableClassesController.cs
@@ -56,7 +56,7 @@ namespace PVGym.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Limit,Duration")] AvailableClass availableClass)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Limit,Duration,Image")] AvailableClass availableClass)
         {
             if (ModelState.IsValid)
             {
@@ -89,7 +89,7 @@ namespace PVGym.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Description,Limit,Duration")] AvailableClass availableClass)
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Description,Limit,Duration,Image")] AvailableClass availableClass)
         {
             if (id != availableClass.Id)
             {
